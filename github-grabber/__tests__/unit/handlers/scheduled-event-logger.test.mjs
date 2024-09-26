890f87b2437d8424e3bf1e96853b5527e18719ca
Chi Kim <chikim79@gmail.com>
2024-09-25 21:14:03 -0700

    rename

--- github-grabber/__tests__/unit/handlers/scheduled-event-logger.test.mjs
+++ github-grabber/__tests__/unit/handlers/scheduled-event-logger.test.mjs
@@ -1,5 +1,5 @@
 // Import scheduledEventLoggerHandler function from scheduled-event-logger.mjs
-import { scheduledEventLoggerHandler } from '../../../src/handlers/scheduled-event-logger.mjs';
+import { scheduledEventLoggerHandler } from '../../../src/handlers/githubGrabber.mjs';
 import { jest } from '@jest/globals';
 
 describe('Test for sqs-payload-logger', function () {
--- github-grabber/src/handlers/githubGrabber.mjs
+++ github-grabber/src/handlers/githubGrabber.mjs
@@ -55,7 +55,7 @@ const processRepo = async (repo) => {
     return null;
 }
 
-export const scheduledEventLoggerHandler = async (event, context) => {
+export const handler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.info(JSON.stringify(event));
